083d923553ce42ccb3434421a05f323543329803
Natasha Sofie <natasha.sofie18@gmail.com>
2025-12-02 11:46:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,29 +4,22 @@ import numpy as np
 from prediction import predict
 
 st.title('Classifying Iris Flowers')
-st.markdown('Toy model to play to classify iris flowers into \
-(setosa, versicolor, virginica) based on their sepal/petal \
-and length/width.')
+st.markdown('Toy model to classify iris flowers into (setosa, versicolor, virginica) based on their sepal/petal length/width.')
 
 st.header("Plant Features")
 col1, col2 = st.columns(2)
 
 with col1:
-st.text("Sepal characteristics")
-sepal_1 = st.slider('Sepal lenght (cm)', 1.0, 8.0, 0.5)
-sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
+    st.text("Sepal characteristics")
+    sepal_l = st.slider('Sepal length (cm)', 1.0, 8.0, 5.0)
+    sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 3.0)
 
 with col2:
-st.text("Pepal characteristics")
-petal_1 = st.slider('Petal lenght (cm)', 1.0, 7.0, 0.5)
-petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
+    st.text("Petal characteristics")
+    petal_l = st.slider('Petal length (cm)', 1.0, 7.0, 4.0)
+    petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 1.0)
 
 st.text('')
 if st.button("Predict type of Iris"):
-result = predict(
-np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
-st.text(result[0])
-
-st.text('')
-st.text('')
-st.markdown(
+    result = predict(np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
+    st.text(f"Prediction Result: {result[0]}")
